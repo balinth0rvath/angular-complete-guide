e332114109d39ef8f17942230e15e1c464025a09
Balint Horvath <horvath0balint@gmail.com>
2017-06-21 21:47:06 +0200

    basic layout

--- src/app/recipie-book/recipie-list/recipie-list.component.ts
+++ src/app/recipie-book/recipie-list/recipie-list.component.ts
@@ -1,3 +1,4 @@
+import { Recepie } from './../recepie.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipie-list.component.css']
 })
 export class RecipieListComponent implements OnInit {
+
+  recipies: Recepie[] = [
+    new Recepie('A Test Recepie', 'This is a simply test',
+      'https://c1.staticflickr.com/3/2547/3862672238_30d378e7d6.jpg'),
+    new Recepie('A Test Recepie', 'This is a simply test',
+      'https://c1.staticflickr.com/3/2547/3862672238_30d378e7d6.jpg'),
+    new Recepie('A Test Recepie', 'This is a simply test',
+      'https://c1.staticflickr.com/3/2547/3862672238_30d378e7d6.jpg')
+
+
+  ];
 
   constructor() { }
 
